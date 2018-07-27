de834ee120ac00d7a8061171180d3bc57eb3d08b
rolim <rolim@DESKTOP-OD1HFAQ.localdomain>
2018-07-27 18:50:09 -0300

    inicio do projeto

---
+++ gerenciamento.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Carros & cia</title>
+        <link rel="stylesheet" href="estilos.css">
+        
+    </head>
+    <body>
+        <header>
+        <?php
+        include 'topo.php';
+        ?>
+        </header>
+      
+        <section>
+          <p>Menu principal de gerenciamento</p>  
+        </section>
+       <nav>
+			<div class="menu_ger">
+				<button id="menua1" class="btmenu">carros</button>
+				<div id="menub1" class="menub">
+					<a href="#" target="_self">novo</a>
+					<a href="#" target="_self">editar</a>
+					<a href="#" target="_self">excluir</a>
+					<a href="#" target="_self">marcas</a>
+				</div>
+			</div>
+			<div class="menu_ger">
+				<button id="menua2" class="btmenu">slider</button>
+				<div id="menub2" class="menub">
+					<a href="#" target="_self">configurar</a>
+				</div>
+			</div>
+			<div class="menu_ger">
+				<button id="menua3" class="btmenu">usuarios</button>
+				<div id="menub3" class="menub">
+					<a href="#" target="_self">novo</a>
+					<a href="#" target="_self">editar</a>
+					<a href="#" target="_self">excluir</a>
+				</div>
+			</div>
+			<div class="menu_ger">
+				<button id="menua4" class="btmenu">logoff</button>
+				<div id="menub4" class="menub">
+					<a href="#" target="_self">sair</a>
+				</div>
+			</div>
+		</nav>
+		
+	</body>
+</html> 
--- logar.php
+++ logar.php
@@ -15,22 +15,37 @@
     
         <section id="main">
             <?php
+                    include './conexao.inc';
+            
                 if(isset($_POST["f_logar"])){
                     $user=$_POST["f_user"];
                     $senha=$_POST["f_senha"];
                     
-                    //MYSQL
-                        //pesquisar user, se existir
-                        //obter user e senha;
+                    $sql="SELECT * FROM tb_colaboradores WHERE username='$user' AND senha='$senha'";
+                    $res= mysqli_query($con, $sql);
+                    $retorno= mysqli_affected_rows($con);
                     
-                    if(($user!="wesley")or($senha!="123")){
+                    if($retorno == 0){
                         echo "<p id='lgErro'>Login incorreto</p>";
                     }else{
                         $chave1="abcdefghijklmnopqrstuvwxyz";
                         $chave2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
                         $chave3="0123456789";
+                        $chave= str_shuffle($chave1.$chave2.$chave3);
+                        $tam= strlen($chave);
+                        $num="";
+                        $qtde= rand(20, 50);
+                        for($i=0 ;$i<$qtde;$i++){
+                            $pos= rand(0, $tam);
+                            $num.= substr($chave,$pos,1);
+                        }
+                        session_start();
+                        $_SESSION['numlogoin']=$num;
+                        $_SESSION['username']=$user;
+                        header("location:gerenciamento.php?num=$num");
                     }
                 }
+                mysqli_close($con);
             ?>
             <form id="f_login" name="f_login" action="logar.php" method="POST">
                 
